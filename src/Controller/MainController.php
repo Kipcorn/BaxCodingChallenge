5ba0d0b33dae98e5b7aaab7b41ee253c937625ef
Dave Everts <dave_e_2013@hotmail.com>
2025-03-23 16:40:06 +0100

    removed asset-mapper componenet

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,9 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController
 {
+    #[Route('/', name: 'Main')]
+    public function index(): Response
+    {
+        return $this->render('app.html.twig');
+    }
 
 
 }
