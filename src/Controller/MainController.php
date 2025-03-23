4e0817326dc04324dbf8bc3c6aab2d2503febaba
Dave Everts <dave_e_2013@hotmail.com>
2025-03-23 16:48:37 +0100

    add css tailwind

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'Main')]
     public function index(): Response
     {
-        return $this->render('app.html.twig');
+        return $this->render('base.html.twig');
     }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,9 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    //enables PostCSS support
+    .enablePostCssLoader();
 ;
 
 module.exports = Encore.getWebpackConfig();
